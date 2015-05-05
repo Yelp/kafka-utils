83210b1f385a608cdab91263e64757f86807a86c
Manpreet Singh <manpreet@yelp.com>
2015-05-05 14:13:57 -0700

    Review comments changs

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from kafka_info import __version__
 
 
 setup(
-    name="kafka_info",
+    name="yelp_kafka_tools",
     version=__version__,
     author="Federico Giraud",
     author_email="fgiraud@yelp.com",
     description="Shows kafka cluster information and metrics",
     packages=["kafka_info", "kafka_info.utils", "kafka_info.commands", "kafka_reassignment"],
-    data_files=[("bash_completion.d", ["bash_completion.d/kafka-info"])],
-    scripts=["kafka-info, kafka-reassignment"],
+    data_files=[("bash_completion.d", ["bash_completion.d/kafka-reassignment"])],  
+    scripts=["kafka-info", "kafka-reassignment"],
     install_requires=[
         "argparse",
         "argcomplete",
