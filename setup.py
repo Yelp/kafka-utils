0fc4567f5d2ed0ede4b22944a16a2e1ee496a0c6
Manpreet Singh <manpreet@yelp.com>
2015-05-04 21:48:22 -0700

    Changes based on review comments

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     author="Federico Giraud",
     author_email="fgiraud@yelp.com",
     description="Shows kafka cluster information and metrics",
-    packages=["kafka_info", "kafka_info.utils", "kafka_info.commands"],
+    packages=["kafka_info", "kafka_info.utils", "kafka_info.commands", "kafka_reassignment"],
     data_files=[("bash_completion.d", ["bash_completion.d/kafka-info"])],
-    scripts=["kafka-info"],
+    scripts=["kafka-info, kafka-reassignment"],
     install_requires=[
         "argparse",
         "argcomplete",
