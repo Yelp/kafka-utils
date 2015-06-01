db1e1d74fc744a089d9162b0a2f4213e80744146
Krishna Kumar Pidaparthi <krp@yelp.com>
2015-06-01 10:56:44 -0700

    Kafka consumer offset management tool - first pass

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Federico Giraud",
     author_email="fgiraud@yelp.com",
     description="Shows kafka cluster information and metrics",
-    packages=["kafka_info", "kafka_info.utils", "kafka_info.commands", "kafka_consumer_manager"],
+    packages=["kafka_info", "kafka_info.utils", "kafka_info.commands", "kafka_consumer_manager", "kafka_consumer_manager.commands"],
     data_files=[("bash_completion.d", ["bash_completion.d/kafka-info"])],
     scripts=["kafka-info"],
     install_requires=[
@@ -17,5 +17,6 @@ setup(
         "argcomplete",
         "kazoo",
         "PyYAML",
+        "yelp-kafka",
     ],
 )
