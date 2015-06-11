31bff8f5bbb7e7005a2de4f6f9ef8dad2e6663e5
Krishna Kumar Pidaparthi <krp@yelp.com>
2015-06-10 18:31:51 -0700

    Remove bash_completition references in consumer-manager

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="fgiraud@yelp.com",
     description="Shows kafka cluster information and metrics",
     packages=["kafka_info", "kafka_info.utils", "kafka_info.commands", "kafka_consumer_manager", "kafka_consumer_manager.commands"],
-    data_files=[("bash_completion.d", ["kafka-info", "kafka-consumer-manager"])],
+    data_files=[("bash_completion.d", ["kafka-info"])],
     scripts=["kafka-info", "kafka-consumer-manager"],
     install_requires=[
         "argparse",
