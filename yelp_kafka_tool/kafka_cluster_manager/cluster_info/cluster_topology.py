ee6c6175814e7fc9617d26daa306fa7ed2bc8df1
Manpreet Singh <manpreet@yelp.com>
2015-08-11 13:58:19 -0700

    Raise exceptions

--- yelp_kafka_tool/kafka_cluster_manager/cluster_info/cluster_topology.py
+++ yelp_kafka_tool/kafka_cluster_manager/cluster_info/cluster_topology.py
@@ -8,7 +8,6 @@ https://docs.google.com/document/d/1qloANcOHkzuu8wYVm0ZAMCGY5Mmb-tdcxUywNIXfQFI
 """
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import print_function
 from __future__ import unicode_literals
 
 import logging
