d225722746bff53046ddeb7f49ce1f44a9b2d8a4
Enrico Canzonieri <enrico@yelp.com>
2016-03-14 17:47:38 -0700

    Cleanup KafkaInterface

--- yelp_kafka_tool/kafka_cluster_manager/util.py
+++ yelp_kafka_tool/kafka_cluster_manager/util.py
@@ -2,71 +2,27 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import logging
-import os
-import subprocess
 from collections import OrderedDict
 
 
-def get_plan(proposed_assignment):
+def assignment_to_plan(assignment):
+    """Convert an assignment to the format used by Kafka to describe a reassignment plan"""
     return {
         'version': 1,
         'partitions':
         [{'topic': t_p[0],
           'partition': t_p[1],
           'replicas': replica
-          } for t_p, replica in proposed_assignment.iteritems()]
+          } for t_p, replica in assignment.iteritems()]
     }
 
 
 class KafkaInterface(object):
-    """This class acts as an interface to interact with kafka-scripts."""
+    """This class acts as an interface to interact with Kafka."""
 
     def __init__(self, script_path=None):
         self._kafka_script_path = script_path
         self.log = logging.getLogger(self.__class__.__name__)
-
-    def run_repartition_cmd(
-        self,
-        zookeeper,
-        brokers,
-        temp_file_name,
-        is_execute=False,
-    ):
-        """Run reassignment kafka command based on given parameters.
-
-        Keywords:
-        temp_file_name: File containing the topic list
-                        or topic-partition layout over brokers,
-                        which helps reassignment script to
-                        generate or execute the plan.
-        is_execute:     Flag symbolizing generation or execution of plan.
-        return:         Generated and Proposed plan or execution status based on
-                        whether command is for generation
-                        or execution respectively.
-        """
-        cmd_params = {}
-        if is_execute:
-            cmd_params['action'] = '--execute'
-            cmd_params['file_flag'] = '--reassignment-json-file'
-        else:
-            cmd_params['action'] = '--generate'
-            cmd_params['file_flag'] = '--topics-to-move-json-file'
-        brokers_str = ','.join(str(e) for e in brokers)
-        reassignment_path = os.path.join(
-            os.path.abspath(self._kafka_script_path),
-            'kafka-reassign-partitions.sh',
-        )
-        cmd = [
-            reassignment_path,
-            cmd_params['action'],
-            '--zookeeper',
-            zookeeper,
-            cmd_params['file_flag'],
-            temp_file_name,
-            '--broker-list',
-            brokers_str,
-        ]
-        return subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
 
     def get_cluster_assignment(self, zk, brokers, topic_ids):
         """Generate the reassignment plan for given zookeeper
