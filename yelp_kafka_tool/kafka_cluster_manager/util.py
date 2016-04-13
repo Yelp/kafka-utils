32890dafacaa595a62de2786124d2384d6c09f24
Enrico Canzonieri <enrico@yelp.com>
2016-04-12 19:08:08 -0700

    Remove KafkaInterface

--- yelp_kafka_tool/kafka_cluster_manager/util.py
+++ yelp_kafka_tool/kafka_cluster_manager/util.py
@@ -1,12 +1,7 @@
-from __future__ import absolute_import
-from __future__ import unicode_literals
-
-import logging
-from collections import OrderedDict
-
-
 def assignment_to_plan(assignment):
-    """Convert an assignment to the format used by Kafka to describe a reassignment plan"""
+    """Convert an assignment to the format used by Kafka to
+    describe a reassignment plan.
+    """
     return {
         'version': 1,
         'partitions':
@@ -15,35 +10,3 @@ def assignment_to_plan(assignment):
           'replicas': replica
           } for t_p, replica in assignment.iteritems()]
     }
-
-
-class KafkaInterface(object):
-    """This class acts as an interface to interact with Kafka."""
-
-    def __init__(self, script_path=None):
-        self._kafka_script_path = script_path
-        self.log = logging.getLogger(self.__class__.__name__)
-
-    def get_cluster_assignment(self, zk):
-        """Generate the reassignment plan for given zookeeper
-        configuration.
-        """
-        plan = zk.get_cluster_plan()
-        assignment = {}
-        for elem in plan['partitions']:
-            assignment[
-                (elem['topic'], elem['partition'])
-            ] = elem['replicas']
-        # assignment map created in sorted order for deterministic solution
-        return OrderedDict(sorted(assignment.items(), key=lambda t: t[0]))
-
-    def execute_plan(self, zk, plan):
-        """Execute the proposed plan.
-
-        Execute the given proposed plan over given
-        zookeeper configuration
-
-        Arguments:
-        plan:   Proposed plan in json format
-        """
-        return zk.execute_plan(plan)
