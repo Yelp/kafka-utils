6770d5640dd541717a5d1fe68c6886ec971984dd
Enrico Canzonieri <enrico@yelp.com>
2016-05-12 12:06:32 -0700

    Change Copyright year

--- tests/kafka_consumer_manager/test_offset_advance.py
+++ tests/kafka_consumer_manager/test_offset_advance.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 Yelp Inc.
+# Copyright 2016 Yelp Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
