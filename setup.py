85dc3e90694019093136a610492b68447d6a0388
Enrico Canzonieri <enrico@yelp.com>
2016-05-12 16:04:23 -0700

    Improve setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+
 from setuptools import find_packages
 from setuptools import setup
 
 from kafka_tools import __version__
+
+
+with open(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "README.rst")
+    )
+) as f:
+    README = f.read()
 
 
 setup(
@@ -25,6 +35,10 @@ setup(
     author_email="team-dist-sys@yelp.com",
     description="Kafka management tools",
     packages=find_packages(exclude=["scripts", "tests"]),
+    url="https://github.com/Yelp/kafka-tools",
+    license="Apache License 2.0",
+    long_description=README,
+    keywords="apache kafka",
     scripts=[
         "scripts/kafka-consumer-manager",
         "scripts/kafka-cluster-manager",
@@ -39,13 +53,14 @@ setup(
         "requests<3.0.0",
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
     ],
 )
