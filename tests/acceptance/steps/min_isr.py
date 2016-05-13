050b7691374c51da60770878d1caa549d9c59480
Alexey Pervushin <alp@yelp.com>
2016-05-13 10:38:17 -0700

    Add kafka-check command

---
+++ tests/acceptance/steps/min_isr.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Yelp Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from behave import then
+from behave import when
+from util import call_cmd
+from util import get_cluster_config
+
+from kafka_tools.util.zookeeper import ZK
+
+
+ISR_CONF_NAME = 'min.insync.replicas'
+CONF_PATH = '/config/topics/'
+
+
+def call_min_isr():
+    cmd = ['kafka-check',
+           '--cluster-type', 'test',
+           '--cluster-name', 'test_cluster',
+           '--discovery-base-path', 'tests/acceptance/config',
+           'min_isr']
+    return call_cmd(cmd)
+
+
+def set_min_isr(topic, min_isr):
+    cluster_config = get_cluster_config()
+    with ZK(cluster_config) as zk:
+        config = zk.get_topic_config(topic)
+        config[ISR_CONF_NAME] = str(min_isr)
+        zk.set_topic_config(topic, config)
+
+
+@when(u'we call the min_isr command')
+def step_impl2(context):
+    context.min_isr_out = call_min_isr()
+
+
+@when(u'we change min.isr settings for a topic to 1')
+def step_impl3(context):
+    set_min_isr(context.topic, 1)
+
+
+@when(u'we change min.isr settings for a topic to 2')
+def step_impl4(context):
+    set_min_isr(context.topic, 2)
+
+
+@then(u'OK will be printed')
+def step_impl5(context):
+    assert context.min_isr_out == 'OK: All replicas in sync.\n', context.min_isr_out
+
+
+@then(u'CRITICAL will be printed')
+def step_impl6(context):
+    error_msg = ("isr=1 is lower than min_isr=2 for {topic}:0\n"
+                 "CRITICAL: 1 partition(s) have the number of "
+                 "replicas in sync that is lower than the specified min ISR.\n").format(
+        topic=context.topic)
+    # return back
+    set_min_isr(context.topic, 1)
+    assert context.min_isr_out == error_msg, context.min_isr_out
---
+++ tests/kafka_check/test_command.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Yelp Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from kafka_tools.kafka_check.commands.command import parse_meta_properties_file
+
+
+META_PROPERTIES_CONTENT = [
+    "#\n",
+    "#Thu May 12 17:59:12 BST 2016\n",
+    "version=0\n",
+    "broker.id=123\n",
+]
+
+
+class TestParseMetaPropertiesFile(object):
+
+    def test_parse_meta_properties_file(self):
+        broker_id = parse_meta_properties_file(META_PROPERTIES_CONTENT)
+
+        assert broker_id == 123
+
+    def test_parse_meta_properties_file_empty(self):
+        broker_id = parse_meta_properties_file([])
+
+        assert broker_id is None
