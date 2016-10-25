3e46220b8bd3f3fb9aa42a1674c0e5f1f486b9c3
Anthony Sandrin <asandrin@yelp.com>
2016-10-25 11:25:00 -0700

    Add weight property to Topic

--- kafka_utils/kafka_cluster_manager/cluster_info/topic.py
+++ kafka_utils/kafka_cluster_manager/cluster_info/topic.py
@@ -26,16 +26,13 @@ class Topic(object):
     :params
         id:                 Name of the given topic
         replication_factor: replication factor of a given topic
-        weight:             Relative load of topic compared to other topics
-                            For future use
         partitions:         List of Partition objects
     """
 
-    def __init__(self, id, replication_factor=0, partitions=None, weight=1.0):
+    def __init__(self, id, replication_factor=0, partitions=None):
         self._id = id
         self._replication_factor = replication_factor
         self._partitions = partitions or set([])
-        self.weight = weight
         self.log = logging.getLogger(self.__class__.__name__)
 
     @property
@@ -49,6 +46,10 @@ class Topic(object):
     @property
     def partitions(self):
         return self._partitions
+
+    @property
+    def weight(self):
+        return sum(partition.weight for partition in self._partitions)
 
     def add_partition(self, partition):
         self._partitions.add(partition)
