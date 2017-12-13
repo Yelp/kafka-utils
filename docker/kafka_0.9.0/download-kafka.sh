04a12c590376ae55f24f3f75dac81cd9ac62aed4
Federico Giraud <fgiraud@yelp.com>
2017-12-13 11:01:09 -0800

    Fix download in itests as well

--- docker/kafka_0.9.0/download-kafka.sh
+++ docker/kafka_0.9.0/download-kafka.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 url="https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
-wget  "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
