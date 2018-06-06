4b0762b7afc6da0cfc2da8947b424da84fd7f0e5
Dong Weicheng <dongw@yelp.com>
2018-06-06 14:34:23 -0700

    Add new acceptance test for kafka-utils with kafka version 1.1

--- docker/kafka_0.8.2/download-kafka.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-url="https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
-wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
