bdd4b408029f7e4a9840808dd98893750218f12c
Manpreet Singh <manpreet@yelp.com>
2018-06-26 21:44:11 -0700

    Upgrade kafka-python version for kafka-utils

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     ],
     install_requires=[
         "cryptography>=1.8.1,<2.0.0",
-        "kafka-python>=1.3.2,<1.3.4",
+        "kafka-python>=1.3.2,<1.5.0",
         "kazoo>=2.0,<3.0.0",
         "PyYAML<4.0.0",
         "requests-futures>0.9.0",
