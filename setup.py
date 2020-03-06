15e8b9787c0cf51922eff762747adebbe69e0505
Jeff Widman <jeff@jeffwidman.com>
2020-03-06 00:05:12 -0800

    Bump to latest `kafka-python` version...
    A number of commands were added to the `KafkaAdminClient` including describing topics, configs, etc but they all require `kafka-python` >= 2.0.0. 2.0.1 was a patch release to fix a minor bug, so bumped to that.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     ],
     install_requires=[
         "humanfriendly>=4.8",
-        "kafka-python>=1.3.2,<1.5.0",
+        "kafka-python>=2.0.1,<3.0.0",
         "kazoo>=2.0,<3.0.0",
         "PyYAML>3.10",
         "pytz>=2014.1",
