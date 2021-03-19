243cab3031f8af41ab4a8cfdce391f5377eba1c4
Lennart Rudolph <lrudolph@yelp.com>
2021-03-19 09:26:50 -0700

    Bump version to 3.9.2

--- kafka_utils/__init__.py
+++ kafka_utils/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Yelp Inc.
+# Copyright 2021 Yelp Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = '3.9.1'
+__version__ = '3.9.2'
